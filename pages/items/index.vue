1619942010f43dc37bd00f5446bdcdf4f0a803a6
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-04-08 18:01:57 +0900

    add items view sample

---
+++ pages/items/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-card header="新着記事一覧">
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          {{ item.title }}
+        </li>
+      </ul>
+    </b-card>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { id: 1, title: 'test' },
+        { id: 2, title: 'test' },
+        { id: 3, title: 'test' }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
