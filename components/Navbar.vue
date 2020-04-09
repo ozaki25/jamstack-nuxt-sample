1fb7beb47f3e5559056a261aa6029b8a6cc988aa
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-04-09 16:38:21 +0900

    update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <b-navbar type="dark" variant="dark">
     <b-navbar-brand href="#">Jamstack Sample</b-navbar-brand>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/items">新着一覧</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </template>
 
